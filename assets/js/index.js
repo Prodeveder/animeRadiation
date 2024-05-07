fc3ad5a266996e4afaa4a293dc25e572f4d852cc
Prodeveder <72999777+Prodeveder@users.noreply.github.com>
2024-05-07 10:08:04 +0200

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -41,7 +41,7 @@ function showSlider(){
     clearInterval(refreshInterval);
     refreshInterval = setInterval(() => {
         next.click();
-    }, 7000)
+    }, 5000)
 }
 
 // click thumbnail
@@ -51,3 +51,31 @@ thumbnails.forEach((thumbnail, index) => {
         showSlider();
     })
 })
+
+// Chart Js
+const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+const yValues = [55, 49, 44, 24, 15];
+const barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+  "#e8c3b9",
+  "#1e7145"
+];
+
+new Chart("myChart", {
+  type: "pie",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "World Wide Wine Production 2018"
+    }
+  }
+});
